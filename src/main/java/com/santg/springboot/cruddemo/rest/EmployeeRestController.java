0bc1bda8655330fb9fbcefeb41b2baf3c7555e33
Santiago Grangetto <santiagograngetto@gmail.com>
2019-07-04 16:49:35 -0300

    Add mapping to add employee
    Add a POST mapping in /employees to add a new employee parsing a json with the data

--- src/main/java/com/santg/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/santg/springboot/cruddemo/rest/EmployeeRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +40,19 @@ public class EmployeeRestController {
 		if (employee == null) {
 			throw new RuntimeException("Employee id not found - " + employeeId);
 		}
+		
+		return employee;
+	}
+	
+	// add mapping for POST /employees to add a new employee
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee employee) {
+		// in case they pass an id in the JSON set the id to 0
+		// to force a save of new employee instead of update
+		employee.setId(0);
+		
+		// save the employee
+		employeeService.save(employee);
 		
 		return employee;
 	}
