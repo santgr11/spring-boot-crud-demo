a825e57d7145e37edf01ddbe2cef76ec2f02170d
Santiago Grangetto <santiagograngetto@gmail.com>
2019-07-04 16:45:23 -0300

    Add find employee
    add a get method to get an employee by its id in /employees/{employeId}

--- src/main/java/com/santg/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/santg/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +27,18 @@ public class EmployeeRestController {
 	@GetMapping("/employees")
 	public List<Employee> findAll() {		
 		return employeeService.findAll();
+	}
+	
+	// add mapping for GET /employees/{employeeId}
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId) {
+		
+		Employee employee = employeeService.findById(employeeId);
+		
+		if (employee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+		
+		return employee;
 	}
 }
